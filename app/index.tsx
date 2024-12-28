e1250109ef2a13f45ed35d14490462366b6f7826
RitikOplify <ritik.ranjan@oplify.in>
2024-12-28 18:32:54 +0530

    Expo-App

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center h-full bg-gray-800">
+      <Text className="text-2xl text-white font-medium">Hello, from Expo!</Text>
+    </View>
+  );
+}
